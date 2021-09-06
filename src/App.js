19b11e7cc395f4ebbe88191a38a03b1ca533b74f
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-09-06 18:34:27 +0530

    web3js setup added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  checkCorrectNetwork,
+  checkWalletAvailable,
+  getMainBalance,
+  getUserAddress,
+} from "./actions/Web3Actions";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({
+    wallet: false,
+    chainId: "not found",
+    address: "Unavailable",
+    balance: "0",
+  });
+
+  const connectButton = async () => {
+    let wallet = await checkWalletAvailable();
+    let address = await getUserAddress();
+    let balance = await getMainBalance();
+    let chainID = await checkCorrectNetwork();
+
+    setData({
+      wallet: wallet,
+      chainId: chainID,
+      address: address,
+      balance: balance,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Account Details</h2>
+      <div className="section">
+        <div className="card">
+          <div className="card-section">
+            <h3>Wallet available</h3>
+            <p>
+              <strong>{data.wallet ? "Yes" : "No"}</strong>
+            </p>
+          </div>
+          <div className="card-section">
+            <h3>Address</h3>
+            <p>
+              <strong>{data.address}</strong>
+            </p>
+          </div>
+          <div className="card-section">
+            <h3>Selected Network Chain ID</h3>
+
+            <p>
+              <strong>{data.chainId}</strong>
+            </p>
+          </div>
+          <div className="card-section">
+            <h3>Main Balance</h3>
+            <p>
+              <strong>{parseFloat(data.balance).toFixed(3)} BNB</strong>
+            </p>
+          </div>
+          <button onClick={connectButton}>Connect Metamask</button>
+        </div>
+      </div>
     </div>
   );
 }
